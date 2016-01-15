3513e0737dc33a2703cc522638a2bb626e7f6e39
Shahab <safaeishahab@yahoo.com>
2016-01-15 20:25:59 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {shahabTFBbotmaster,19511827},
+    sudo_users = {shahabTFBbotmaster,119989724},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
